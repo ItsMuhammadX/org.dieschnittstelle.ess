903ea17765aea1377589ae507fdfbd2e782acd53
Muhammad <s92258@bht-berlin.de>
2026-06-02 13:45:36 +0200

    feat: MIP+JPA1 done

---
+++ org.dieschnittstelle.ess.mip.components.erp.impl/src/main/java/org/dieschnittstelle/ess/mip/components/erp/crud/impl/ProductCRUDImpl.java
@@ -0,0 +1,76 @@
+package org.dieschnittstelle.ess.mip.components.erp.crud.impl;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
+import jakarta.transaction.Transactional;
+import org.dieschnittstelle.ess.entities.erp.AbstractProduct;
+import org.dieschnittstelle.ess.entities.erp.Campaign;
+import org.dieschnittstelle.ess.entities.erp.IndividualisedProductItem;
+import org.dieschnittstelle.ess.entities.erp.ProductType;
+import org.dieschnittstelle.ess.mip.components.erp.crud.api.ProductCRUD;
+import org.dieschnittstelle.ess.utils.interceptors.Logged;
+
+import java.util.List;
+
+
+@ApplicationScoped
+@Transactional
+@Logged
+public class ProductCRUDImpl implements ProductCRUD {
+
+    @Inject
+    @EntityManagerProvider .ERPDataAccessor
+    private EntityManager em;
+
+    @Override
+    public AbstractProduct createProduct(AbstractProduct prod) {
+        //em.persist(prod);
+        return prod;
+    }
+
+    @Override
+    public List<AbstractProduct> readAllProducts() {
+        IndividualisedProductItem prod = new IndividualisedProductItem();
+        prod.setName("Kirschplunder");
+        prod.setProductType(ProductType.PASTRY);
+        return List.of(prod);
+        //Query query = em.createQuery("SELECT DISTINCT p FROM AbstractProduct");
+        //return query.getResultList();
+    }
+
+    @Override
+    public AbstractProduct updateProduct(AbstractProduct update) {
+        //return em.merge(update);
+        return null;
+    }
+
+    @Override
+    public AbstractProduct readProduct(long productID) {
+        //return em.find(AbstractProduct.class, productID);
+        return null;
+    }
+    @Override
+    public boolean deleteProduct(long productID) {
+        try {
+            AbstractProduct prod = this.readProduct(productID);
+            if (prod != null) {
+                //em.remove(prod);
+                return true;
+            } else {
+                return false;
+            }
+        }
+        catch (Exception e) {
+            return false;
+        }
+
+    }
+
+    @Override
+    public List<Campaign> getCampaignsForProduct(long productID) {
+        return List.of();
+    }
+
+}
