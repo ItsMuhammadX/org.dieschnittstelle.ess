6f4977a7e41eb0134c949b1fb29b053a4c7ce485
Muhammad <s92258@bht-berlin.de>
2026-06-30 14:46:16 +0200

    feat: PAT1 start, not finished

--- org.dieschnittstelle.ess.mip.components.shopping.api/src/main/java/org/dieschnittstelle/ess/mip/components/shopping/api/PurchaseService.java
+++ org.dieschnittstelle.ess.mip.components.shopping.api/src/main/java/org/dieschnittstelle/ess/mip/components/shopping/api/PurchaseService.java
@@ -1,8 +1,55 @@
 package org.dieschnittstelle.ess.mip.components.shopping.api;
 
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
 // TODO: PAT1: this is the interface to be provided as a rest service if rest service access is used
+@Path("/purchase")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+
 public interface PurchaseService {
 
-	void purchaseCartAtTouchpointForCustomer(long shoppingCartId, long touchpointId, long customerId) throws ShoppingException;
+	public static class PurchaseDTO {
+		public long shoppingCartId;
+		public long touchpointId;
+		public long customerId;
+
+		public PurchaseDTO(long shoppingCartId, long touchpointId, long customerId) {
+			this.shoppingCartId = shoppingCartId;
+			this.touchpointId = touchpointId;
+			this.customerId = customerId;
+		}
+
+		public long getShoppingCartId() {
+			return shoppingCartId;
+		}
+
+		public void setShoppingCartId(long shoppingCartId) {
+			this.shoppingCartId = shoppingCartId;
+		}
+
+		public long getTouchpointId() {
+			return touchpointId;
+		}
+
+		public void setTouchpointId(long touchpointId) {
+			this.touchpointId = touchpointId;
+		}
+
+		public long getCustomerId() {
+			return customerId;
+		}
+
+		public void setCustomerId(long customerId) {
+			this.customerId = customerId;
+		}
+	}
+
+	@POST
+	void purchaseCartAtTouchpointForCustomer(PurchaseDTO purchaseDTO) throws ShoppingException;
 	
 }
